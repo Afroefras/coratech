10269f7664ba1de4f55595637add011b9dd36488
EF <efraisma.ef7@gmail.com>
2024-02-04 16:14:59 -0600

    Update extract.py
    Get all pairs of mobile-digital audio files

--- extract.py
+++ extract.py
@@ -1,14 +1,30 @@
-from typing import Tuple
+from typing import List, Tuple
 from pathlib import Path, PosixPath
 
 
 def find_pair(
-    mobile_dir: Path, mobile_audio_name: str, digital_dir: Path, digital_ext: str
+    mobile_audio_dir: PosixPath, digital_dir: Path, digital_ext: str
 ) -> Tuple[PosixPath, PosixPath]:
-    mobile_audio_dir = mobile_dir.joinpath(mobile_audio_name)
     mobile_audio_name = mobile_audio_dir.stem
 
     digital_audio_name = mobile_audio_name + digital_ext
     digital_audio_dir = digital_dir.joinpath(digital_audio_name)
 
     return mobile_audio_dir, digital_audio_dir
+
+
+def load_pairs(
+    mobile_dir: str, mobile_ext: str, digital_dir: str, digital_ext: str
+) -> List[Tuple[PosixPath, PosixPath]]:
+    mobile_dir = Path(mobile_dir)
+    digital_dir = Path(digital_dir)
+
+    audios = []
+    for audio in mobile_dir.glob(f"*{mobile_ext}"):
+        mobile, digital = find_pair(
+            mobile_audio_dir=audio, digital_dir=digital_dir, digital_ext=digital_ext
+        )
+
+        audios.append((mobile, digital))
+
+    return audios
