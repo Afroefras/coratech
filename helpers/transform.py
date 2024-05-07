d2b94246c2cb45c1d25d3a52c705666225e6f050
EF <66633690+Afroefras@users.noreply.github.com>
2024-05-06 18:25:19 -0600

    Step by step notebook to show how TrimAfterClicker works under the hood

--- helpers/transform.py
+++ helpers/transform.py
@@ -45,10 +45,9 @@ def min_max_scale(x: Tensor) -> Tensor:
     return scaled
 
 
-class AlignAudios:
+class TrimAfterClicker:
     def __init__(self) -> None:
-        """Initializes the AlignAudios class."""
-
+        """Initializes the TrimAfterClicker class."""
         pass
 
     def load_audio(self, audio_dir: str) -> Tuple[Tensor, Tensor]:
@@ -77,35 +76,94 @@ class AlignAudios:
 
         scaled = scaler(audio)
         return scaled
-    
+
     def get_frequencies(self, audio: Tensor, sample_rate: int) -> Tensor:
-        frequencies = fft.fftfreq(audio.size(1), d=1/sample_rate)
+        """Calculates the frequencies of the audio tensor.
+
+        Args:
+            audio: The audio tensor.
+            sample_rate: The sample rate of the audio.
+
+        Returns:
+            A tensor containing the frequencies.
+        """
+
+        frequencies = fft.fftfreq(audio.size(1), d=1 / sample_rate)
         return frequencies
 
     def get_magnitudes(self, audio: Tensor) -> Tensor:
+        """Calculates the magnitudes of the audio tensor.
+
+        Args:
+            audio: The audio tensor.
+
+        Returns:
+            A tensor containing the magnitudes.
+        """
+
         magnitudes = fft.fft(audio, dim=1).abs()
         return magnitudes
-    
-    def half_magnitudes_and_frequencies(self, magnitudes: Tensor, frequencies: Tensor) -> Tensor:
-        half_magnitudes = magnitudes[:, :magnitudes.size(1) // 2]
-        half_frequencies = frequencies[:magnitudes.size(1) // 2]
+
+    def half_magnitudes_and_frequencies(
+        self, magnitudes: Tensor, frequencies: Tensor
+    ) -> Tensor:
+        """Calculates the half magnitudes and frequencies of the
+            audio tensor because FFT is symmetrical.
+
+        Args:
+            magnitudes: The magnitudes of the audio tensor.
+            frequencies: The frequencies of the audio tensor.
+
+        Returns:
+            A tuple containing the half magnitudes and frequencies.
+        """
+
+        half_magnitudes = magnitudes[:, : magnitudes.size(1) // 2]
+        half_frequencies = frequencies[: magnitudes.size(1) // 2]
         return half_magnitudes, half_frequencies
-    
-    def sort_frequencies(self, half_magnitudes: Tensor, half_frequencies: Tensor) -> Tensor:
-        sorted_magnitudes = half_magnitudes.argsort()[::-1]
+
+    def sort_frequencies(
+        self, half_magnitudes: Tensor, half_frequencies: Tensor
+    ) -> Tensor:
+        """Sorts the half frequencies of the audio tensor.
+
+        Args:
+            half_magnitudes: The half magnitudes of the audio tensor.
+            half_frequencies: The half frequencies of the audio tensor.
+
+        Returns:
+            A tensor containing the sorted half frequencies.
+        """
+
+        sorted_magnitudes = half_magnitudes.argsort(descending=True)
         sorted_frequencies = half_frequencies[sorted_magnitudes]
         return sorted_frequencies
-    
-    def get_frequency_percentile(self, audio: Tensor, sample_rate: int, percentile: float) -> Tensor:
+
+    def get_frequency_percentile(
+        self, audio: Tensor, sample_rate: int, percentile_num: float
+    ) -> float:
+        """Calculates the frequency percentile of the audio tensor.
+
+        Args:
+            audio: The audio tensor.
+            sample_rate: The sample rate of the audio.
+            percentile_num: The percentile to calculate.
+
+        Returns:
+            The float frequency percentile.
+        """
+
         frequencies = self.get_frequencies(audio, sample_rate)
         magnitudes = self.get_magnitudes(audio)
 
-        half_magnitudes, half_frequencies = self.half_magnitudes_and_frequencies(magnitudes, frequencies)
+        half_magnitudes, half_frequencies = self.half_magnitudes_and_frequencies(
+            magnitudes, frequencies
+        )
         sorted_frequencies = self.sort_frequencies(half_magnitudes, half_frequencies)
 
-        freq_percentile = percentile(sorted_frequencies, percentile)
+        freq_percentile = percentile(sorted_frequencies, percentile_num)
         return freq_percentile
-    
+
     def highpass_filter(self, audio: Tensor, sample_rate: int, cutoff: int) -> Tensor:
         """Applies a highpass filter to the audio tensor.
 
@@ -152,44 +210,41 @@ class AlignAudios:
             The trimmed audio tensor.
         """
 
-        begin_at = peaks[n_peak] + sample_rate // 60
+        begin_at = peaks[n_peak] + sample_rate // 20
         trimmed = audio[:, begin_at:]
         return trimmed
 
     def transform(
         self,
         audio_dir: str,
-        highpass_percentile: 90,
-        peak_threshold: float = 0.7,
-        n_peak: int = -1,
     ) -> Tensor:
         """Transforms the audio tensor using the following steps:
 
         1. Load the audio file.
         2. Scale the audio tensor.
-        3. Apply a highpass filter to the audio tensor.
+        3. Calculate the frequency percentile
+        3. Apply a highpass filter given the previous percentile.
         4. Find the peaks in the audio tensor.
         5. Trim the audio tensor to the given peak.
         6. Scale the trimmed audio tensor to the range [0, 1].
 
         Args:
             audio_dir: The directory of the audio file.
-            cutoff: The cutoff frequency of the highpass filter.
-            threshold: The threshold for peak detection.
-            n_peak: The index of the peak to trim to.
 
         Returns:
             The transformed audio tensor.
         """
 
         audio, sample_rate = self.load_audio(audio_dir)
-        audio = self.scale_audio(audio, standard_scale)
+        scaled_audio = self.scale_audio(audio, standard_scale)
 
-        cutoff = self.get_frequency_percentile(audio, sample_rate, highpass_percentile)
-        audio = self.highpass_filter(audio, sample_rate, cutoff)
+        cutoff = self.get_frequency_percentile(scaled_audio, sample_rate, 90)
+        filtered_audio = self.highpass_filter(scaled_audio, sample_rate, cutoff)
 
-        peaks = self.find_peaks(audio, peak_threshold)
-        trimmed = self.trim_audio(audio, sample_rate, peaks, n_peak)
+        peak_threshold = percentile(filtered_audio, 99.99)
+        peaks = self.find_peaks(filtered_audio, peak_threshold)
 
+        trimmed = self.trim_audio(audio, sample_rate, peaks, n_peak=-1)
         final_audio = min_max_scale(trimmed)
+
         return final_audio, sample_rate
