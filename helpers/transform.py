aa25e47832643ce863d5555b334547a6c0359f9b
EF <66633690+Afroefras@users.noreply.github.com>
2024-05-23 17:45:26 -0600

    watching closer how clicker sound behaves

--- helpers/transform.py
+++ helpers/transform.py
@@ -96,60 +96,7 @@ class TrimAfterClicker:
         frequencies = fft.fftfreq(audio.size(1), d=1 / sample_rate)
         return frequencies
 
-    def get_magnitudes(self, audio: Tensor) -> Tensor:
-        """
-        Calculates the magnitudes of the audio tensor.
-
-        Args:
-            audio: The audio tensor.
-
-        Returns:
-            A tensor containing the magnitudes.
-        """
-
-        magnitudes = fft.fft(audio, dim=1).abs()
-        return magnitudes
-
-    def half_magnitudes_and_frequencies(
-        self, magnitudes: Tensor, frequencies: Tensor
-    ) -> Tensor:
-        """
-        Calculates the half magnitudes and frequencies of the
-            audio tensor because FFT is symmetrical.
-
-        Args:
-            magnitudes: The magnitudes of the audio tensor.
-            frequencies: The frequencies of the audio tensor.
-
-        Returns:
-            A tuple containing the half magnitudes and frequencies.
-        """
-
-        half_magnitudes = magnitudes[:, : magnitudes.size(1) // 2]
-        half_frequencies = frequencies[: magnitudes.size(1) // 2]
-        return half_magnitudes, half_frequencies
-
-    def sort_frequencies(
-        self, half_magnitudes: Tensor, half_frequencies: Tensor
-    ) -> Tensor:
-        """
-        Sorts the half frequencies of the audio tensor.
-
-        Args:
-            half_magnitudes: The half magnitudes of the audio tensor.
-            half_frequencies: The half frequencies of the audio tensor.
-
-        Returns:
-            A tensor containing the sorted half frequencies.
-        """
-
-        sorted_magnitudes = half_magnitudes.argsort(descending=True)
-        sorted_frequencies = half_frequencies[sorted_magnitudes]
-        return sorted_frequencies
-
-    def get_frequency_percentile(
-        self, audio: Tensor, sample_rate: int, percentile_num: float
-    ) -> float:
+    def get_frequency_percentile(self, frequencies: Tensor, percentile_num: float) -> float:
         """
         Calculates the frequency percentile of the audio tensor.
 
@@ -161,16 +108,7 @@ class TrimAfterClicker:
         Returns:
             The float frequency percentile.
         """
-
-        frequencies = self.get_frequencies(audio, sample_rate)
-        magnitudes = self.get_magnitudes(audio)
-
-        half_magnitudes, half_frequencies = self.half_magnitudes_and_frequencies(
-            magnitudes, frequencies
-        )
-        sorted_frequencies = self.sort_frequencies(half_magnitudes, half_frequencies)
-
-        freq_percentile = percentile(sorted_frequencies, percentile_num)
+        freq_percentile = percentile(frequencies, percentile_num)
         return freq_percentile
 
     def highpass_filter(self, audio: Tensor, sample_rate: int, cutoff: int) -> Tensor:
@@ -251,7 +189,8 @@ class TrimAfterClicker:
         audio, sample_rate = self.load_audio(audio_dir)
         scaled_audio = self.scale_audio(audio, standard_scale)
 
-        cutoff = self.get_frequency_percentile(scaled_audio, sample_rate, 90)
+        frequencies = self.get_frequencies(audio, sample_rate)
+        cutoff = self.get_frequency_percentile(frequencies, 99)
         filtered_audio = self.highpass_filter(scaled_audio, sample_rate, cutoff)
 
         peak_threshold = percentile(filtered_audio, 99.99)
