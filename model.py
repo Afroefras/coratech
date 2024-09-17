6c7f1828902ff37eb7ac13f3239a584aba6ed88f
EF <efraisma.ef7@gmail.com>
2024-09-17 13:00:24 -0600

    First model architecture template

--- model.py
+++ model.py
@@ -1,10 +1,12 @@
 import torch
-from torch import Tensor
+import torch.nn as nn
 from pathlib import Path
 from typing import List, Tuple
-import torch.nn.functional as F
 from torch.utils.data import Dataset
+from pytorch_lightning import LightningModule
 from helpers.audio_utils import standard_scale
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
 
 class CoraTechDataset(Dataset):
     def __init__(self, base_dir: Path, chunk_secs: float):
@@ -14,34 +16,94 @@ class CoraTechDataset(Dataset):
     def __len__(self) -> int:
         return len(self.data)
 
-    def make_heartbeats_chunks(self, audio: torch.Tensor, sample_rate: int, chunk_secs: float) -> List[Tensor]:
+    def make_heartbeats_chunks(
+        self, audio: torch.Tensor, sample_rate: int, chunk_secs: float
+    ) -> List[torch.Tensor]:
         chunk_size = int(sample_rate * chunk_secs)
         chunks = torch.split(audio, chunk_size, dim=-1)
         chunks = list(chunks)
 
         last_chunk_pad_size = chunk_size - chunks[-1].shape[-1]
         if last_chunk_pad_size > 0:
-            chunks[-1] = F.pad(chunks[-1], (0, last_chunk_pad_size))
+            chunks[-1] = nn.functional.pad(chunks[-1], (0, last_chunk_pad_size))
 
         return chunks
-    
+
     def _load_data(self, chunk_secs: float) -> None:
         self.data = []
-        self.data_dir = list(self.base_dir.glob('*.pt'))
+        self.data_dir = list(self.base_dir.glob("*.pt"))
 
         for path in self.data_dir:
             mobile, stethos, sample_rate = torch.load(path)
 
             mobile_chunks = self.make_heartbeats_chunks(mobile, sample_rate, chunk_secs)
-            stethos_chunks = self.make_heartbeats_chunks(stethos, sample_rate, chunk_secs)
-            
+            stethos_chunks = self.make_heartbeats_chunks(
+                stethos, sample_rate, chunk_secs
+            )
+
             chunks = list(zip(mobile_chunks, stethos_chunks))
             self.data.extend(chunks)
 
-    def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor]:
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
         mobile, stethos = self.data[idx]
 
         mobile = standard_scale(mobile)
         stethos = standard_scale(stethos)
-        
+
         return mobile, stethos
+
+
+class CoraDenoiser(LightningModule):
+    def __init__(self, input_size: int):
+        super().__init__()
+        self.lstm1 = nn.LSTM(
+            input_size,
+            hidden_size=2048,
+            num_layers=1,
+            batch_first=True,
+            bidirectional=False,
+        )
+        self.lstm2 = nn.LSTM(
+            2048, hidden_size=2048, num_layers=1, batch_first=True, bidirectional=False
+        )
+        self.dense1 = nn.Linear(2048, 2048)
+        self.dense2 = nn.Linear(2048, 1024)
+        self.dense3 = nn.Linear(1024, 1024)
+        self.output_layer = nn.Linear(1024, input_size)
+
+    def forward(self, x):
+        lstm_output1, _ = self.lstm1(x)
+        lstm_output2, _ = self.lstm2(lstm_output1)
+        lstm_output2 = lstm_output2[:, -1, :].unsqueeze(
+            dim=1
+        )  # Get the output of the last time step
+
+        dense1_output = self.dense1(lstm_output2)
+        dense2_output = self.dense2(dense1_output)
+        dense3_output = self.dense3(dense2_output)
+        output = self.output_layer(dense3_output)
+        return output
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        predictions = self(x)
+        loss = nn.MSELoss()(predictions, y)
+        self.log("train_loss", loss, on_epoch=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        predictions = self(x)
+        loss = nn.MSELoss()(predictions, y)
+        self.log("val_loss", loss, on_epoch=True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        scheduler = {
+            "scheduler": ReduceLROnPlateau(optimizer, patience=3),
+            "monitor": "val_loss",
+            "interval": "epoch",
+            "frequency": 1,
+        }
+        return [optimizer], [scheduler]
